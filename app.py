5f7c5a8a66b97fe3cf5a0b89cc8a4795e21bbe30
turinglabsorg <sebastiano.cataudo@gmail.com>
2023-05-26 11:34:05 +0200

    add cors and json response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 import requests
 import time
 from rembg import remove, new_session
@@ -9,28 +9,43 @@ app = Flask(__name__)
 def hello():
     return 'API is working!'
     
-@app.route('/remove', methods=['POST'])
+@app.route('/remove', methods=['POST', 'OPTIONS'])
 def remove_bg():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        json = request.json
-        now = str(time.time())
-        url = json['url']
-        data = requests.get(url).content
-        f = open('./origin/' + now + '.jpg','wb')
-        f.write(data)
-        f.close()
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "POST":
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            json = request.json
+            now = str(time.time())
+            url = json['url']
+            data = requests.get(url).content
+            f = open('./origin/' + now + '.jpg','wb')
+            f.write(data)
+            f.close()
 
-        input_path = './origin/' + now + '.jpg'
-        output_path = './static/' + now + '.png'
+            input_path = './origin/' + now + '.jpg'
+            output_path = './static/' + now + '.png'
+            res = {}
+            res['result'] = now + '.png'
+            with open(input_path, 'rb') as i:
+                with open(output_path, 'wb') as o:
+                    input = i.read()
+                    model_name = "u2net_human_seg"
+                    session = new_session(model_name)
+                    output = remove(input, session=session)
+                    o.write(output)
+            return _corsify_actual_response(jsonify(res))
+        else:
+            return 'Content-Type not supported!'
+    
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
-        with open(input_path, 'rb') as i:
-            with open(output_path, 'wb') as o:
-                input = i.read()
-                model_name = "u2net_human_seg"
-                session = new_session(model_name)
-                output = remove(input, session=session)
-                o.write(output)
-        return now + '.png'
-    else:
-        return 'Content-Type not supported!'
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
